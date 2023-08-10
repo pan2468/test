dd3b53bbc99e69860096975b679326bfd24068a5
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-08-11 00:45:48 +0900

    ds
    ds

---
+++ back_Java/githubupload/node/execute.js
@@ -0,0 +1,12 @@
+const { stdout } = require('process');
+
+const execute = require('child_process').exec;
+
+execute("ls", (err, stdout)=>{
+
+    if (err) {
+        return err;
+    }
+
+    console.log(stdout);
+})
---
+++ back_Java/githubupload/node/lib.js
@@ -0,0 +1,8 @@
+
+
+module.exports = { 
+
+    cal: function(val1, val2) {
+        return val1 + val2;
+    }
+}
